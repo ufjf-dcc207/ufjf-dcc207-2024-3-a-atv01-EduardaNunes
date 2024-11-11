7d143ed74e0afe18506b868a9f77acb6605bb304
Duda Nunes <eduarda.nunes@estudante.ufjf.br>
2024-11-11 20:48:18 -0300

    Added more Disciplina Components to App

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ export default function App() {
   return (
     <>
       <h1>Hello, World</h1>
-      <Disciplina codigo="DCC2017" nome="Desenvolvimento Web Front End" />
+      <Disciplina codigo="DCC202" nome="Desenvolvimento Web I" />
+      <Disciplina codigo="DCC206" nome="Desenvolvimento Web II" />
+      <Disciplina codigo="DCC207" nome="Desenvolvimento Web Front End" />
     </>
   )
 }
