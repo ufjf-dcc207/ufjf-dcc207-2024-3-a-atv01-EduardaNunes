120810e22f9c6e6b15730424caf6d425a42282bb
Duda Nunes <eduarda.nunes@estudante.ufjf.br>
2024-11-11 20:40:45 -0300

    Created Component Disciplina

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import Disciplina from "./Disciplina.tsx"
+
 export default function App() {
-  return (<h1>Hello, World</h1>)
+  return (
+    <>
+      <h1>Hello, World</h1>
+      <Disciplina />
+    </>
+  )
 }
